b2fa34876569e3ffe9eeac62c0b9f4cf8a7c08dc
David Atanda <ololadedavid15@gmail.com>
2023-08-26 11:06:34 +0100

    app routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Email from './components/register/Email';
+import Password from './components/register/Password';
+import PhoneNumber from './components/register/PhoneNumber';
+import ChooseCountry from './components/register/ChooseCountry';
+import Sidebar from './pages/Sidebar';
 
 function App() {
   return (
@@ -11,6 +16,13 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/register'>
+            <Route path='/register/email' element={<Email /> } />
+            <Route path='/register/password' element={<Password />} />
+            <Route path='/register/phone-num' element={<PhoneNumber />} />
+            <Route path='/register/choose-country' element={<ChooseCountry />} />
+          </Route>
+          <Route path='/user' element={<Sidebar />} ></Route>
         </Routes>
       </BrowserRouter>
     </div>
