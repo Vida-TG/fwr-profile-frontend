a385e2210568807e9bbbd3e8f01c0e83beb6db38
David Atanda <ololadedavid15@gmail.com>
2023-08-26 23:42:09 +0100

    forgot password

---
+++ src/components/password-reset/ForgotPassword.js
@@ -0,0 +1,38 @@
+import { Alert, FormControl, InputLabel, OutlinedInput } from "@mui/material";
+import React, { useState } from "react";
+
+const ForgotPassword = ()=>{
+    const [error, setError] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    return (
+        <div className="forgot-password d-flex justify-content-center mx-auto pb-5">
+            <div className="col-lg-3 col-md-6 mt-sm-5 pt-md-0 px-md-0 px-4">
+                <div className="d-flex justify-content-center mt-4 mt-md-0">
+                    <img src={require('../../assets/9jaWivesLogo.png')} />
+                </div>
+                <p className="fs-4 pt-5 fw-bold-0">Forgot your password?</p>
+                {
+                    error !== ''
+                    &&
+                    <Alert severity="error" className="mb-2">
+                        {error}
+                    </Alert>
+                }
+                <p className="fs-7">
+                    Enter the Email you used in registering and we will help you reset your password
+                </p>
+                <FormControl variant="outlined" className="w-100 my-2" color="warning">
+                    <InputLabel className="text-white">Email Address</InputLabel>
+                    <OutlinedInput label='Email Address' className="text-white" />
+                </FormControl>
+                <div className="d-flex justify-content-center pb-5 mt-2">
+                    <button disabled="isLoading" type="submit" className={isLoading ? 'border border-fwr mb-5 fw-bold mx-auto text-white mx-2 px-5 py-3 rounded-pill w-75' : 'bg-fwr mb-5 fw-bold mx-auto text-white mx-2 px-5 py-3 rounded-pill w-75'}>
+                        {isLoading ? 'Please wait...' : 'Continue'}
+                    </button>
+                </div>        
+            </div>
+        </div>
+    )
+}
+
+export default ForgotPassword
