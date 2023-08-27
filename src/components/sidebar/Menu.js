ba1df6eeae06669aeecb38e1e59a5aed5da36b24
David Atanda <ololadedavid15@gmail.com>
2023-08-27 18:52:20 +0100

    sidebar changes

--- src/components/sidebar/Menu.js
+++ src/components/sidebar/Menu.js
@@ -13,11 +13,11 @@ const Menu = ()=>{
         <>                        
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton onClick={()=>navigate('/user/statement-of-account')} className={location.pathname == '/user/statement-of-account' ? 'bg-active' : ''} >
+                    <ListItemButton onClick={()=>navigate('/user/statement-of-account')} className={location.pathname == '/user/statement-of-account' ? 'active' : ''} >
                         <ListItemIcon>
                         </ListItemIcon>
                         <ListItemText>
-                            <p className='my-0'>
+                            <p className={location.pathname == '/user/statement-of-account' ? 'fw-bolder my-0' : 'my-0'}>
                                  Statement of Account
                             </p>
                         </ListItemText>
@@ -26,11 +26,11 @@ const Menu = ()=>{
             </List>
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton onClick={()=>navigate('/user/instalment-plans')} className={location.pathname == '/user/instalment-plans' ? 'bg-active' : ''} >
+                    <ListItemButton onClick={()=>navigate('/user/instalment-plans')} className={location.pathname == '/user/instalment-plans' ? 'active' : ''} >
                         <ListItemIcon>
                         </ListItemIcon>
                         <ListItemText>
-                            <p className='my-0'>
+                            <p className={location.pathname == '/user/instalment-plans' ? 'fw-bolder my-0' : 'my-0'}>
                              Instalment Plans
                             </p>
                         </ListItemText>
@@ -39,11 +39,11 @@ const Menu = ()=>{
             </List>
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton  onClick={()=>navigate('/user/uploaded-docs')} className={location.pathname == '/user/uploaded-docs' ? 'bg-active' : ''} >
+                    <ListItemButton  onClick={()=>navigate('/user/uploaded-docs')} className={location.pathname == '/user/uploaded-docs' ? 'active' : ''} >
                         <ListItemIcon>
                         </ListItemIcon>
                         <ListItemText>
-                            <p className='my-0'>
+                            <p className={location.pathname == '/user/uploaded-docs' ? 'fw-bolder my-0' : 'my-0'}>
                              Uploaded Document
                             </p>
                         </ListItemText>
@@ -52,11 +52,11 @@ const Menu = ()=>{
             </List>
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton  onClick={()=>navigate('/user/bank-cards')} className={location.pathname == '/user/bank-cards' ? 'bg-active' : ''} >
+                    <ListItemButton  onClick={()=>navigate('/user/bank-cards')} className={location.pathname == '/user/bank-cards' ? 'active' : ''} >
                         <ListItemIcon>
                         </ListItemIcon>
                         <ListItemText>
-                            <p className='my-0'>
+                            <p className={location.pathname == '/user/bank-cards' ? 'fw-bolder my-0' : 'my-0'}>
                                  Bank Card
                             </p>
                         </ListItemText>
@@ -65,11 +65,11 @@ const Menu = ()=>{
             </List>
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton  onClick={()=>navigate('/user/address')} className={location.pathname == '/user/address' ? 'bg-active' : ''} >
+                    <ListItemButton  onClick={()=>navigate('/user/address')} className={location.pathname == '/user/address' ? 'active' : ''} >
                         <ListItemIcon>
                         </ListItemIcon>
                         <ListItemText>
-                            <p className='my-0'>
+                            <p className={location.pathname == '/user/address' ? 'fw-bolder my-0' : 'my-0'}>
                                  Addresses
                             </p>
                         </ListItemText>
