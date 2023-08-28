78e623b78abc2f10e86dd8a8c670136298c22f5c
David Atanda <ololadedavid15@gmail.com>
2023-08-28 20:21:10 +0100

    remote link

--- src/reducers/api.js
+++ src/reducers/api.js
@@ -1,6 +1,6 @@
 const initState = {
-    // uri: 'https://foreignwr-server.onrender.com/'
-    uri: 'http://localhost:5050/'
+    uri: 'https://foreignwr-server.onrender.com/'
+    // uri: 'http://localhost:5050/'
 }
 
 const ApiReducer = (state=initState, action)=>{
