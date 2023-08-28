3ee14ce721e27dfc023c1de65b0dcc4d60317322
David Atanda <ololadedavid15@gmail.com>
2023-08-28 17:27:27 +0100

    schemas

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,8 +1,20 @@
 import * as yup from 'yup'
+const passwordRules = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}/
+const phonenumberRules = /(^[+]\d{13}$)/
 
 export const loginSchema = yup.object().shape({
     email: yup.string().required('Required'),
     password: yup.string().required('Required')
 })
 
-// export
+export const emailSchema = yup.object().shape({
+    email: yup.string().email('Invalid Email Address').required('Required')
+})
+
+export const passwordSchema = yup.object().shape({
+    password: yup.string().matches(passwordRules, { message: 'Please create a stronger password' }).required('Required')
+})
+
+export const phonenumberSchema = yup.object().shape({
+    phonenum: yup.string().matches(phonenumberRules, { message: 'Enter your country code' }).required('Required')
+})
